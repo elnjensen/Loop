31ad5580188014ee2030026d159752b5394abf38
elnjensen <ejensen1@swarthmore.edu>
2019-08-17 13:19:00 -0400

    Monospaced fonts, handle offscreen correctly

--- WatchApp Extension/Controllers/WorkoutHUDController.swift
+++ WatchApp Extension/Controllers/WorkoutHUDController.swift
@@ -6,10 +6,20 @@
 //  Copyright © 2019 LoopKit Authors. All rights reserved.
 //
 
+// Simple Watch face in Loop to display a stopwatch along with BG info
+// to make exercise easier without switching apps.
+// Note that the "timer" variable below isn't what keeps track of the
+// elapsed time - it is a timer for display updates, so it is turned off
+// when the face isn't displayed.  It is the timerStart variable that
+// marks the start of the time interval, and we calculate and display offsets
+// from that.
+
+
 import WatchKit
 import WatchConnectivity
 import LoopKit
 import LoopCore
+import UIKit
 
 final class WorkoutHUDController: HUDInterfaceController {
     
@@ -21,31 +31,34 @@ final class WorkoutHUDController: HUDInterfaceController {
         pauseResumeTimer()
     }
     
-    var timerStart = NSDate()
+    var timerStart = Date()
     var savedTime: TimeInterval = 0.0
     var timer = Timer()
-    let startColor = UIColor(red: 35/255, green: 124/255, blue: 0/255, alpha: 1.0)
-    let stopColor = UIColor(red: 160/255, green: 40/255, blue: 0/255, alpha: 1.0)
+    let startColor = UIColor(red: 35/255, green: 124/255, blue: 10/255, alpha: 0.9)
+    let stopColor = UIColor(red: 160/255, green: 40/255, blue: 0/255, alpha: 0.9)
     
     func startTimer(offset: TimeInterval) {
-        timerStart = NSDate() // initialize to now
-        timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: {timer in self.elapsedTimeLabel.setText(self.timeString(time: abs(self.timerStart.timeIntervalSinceNow) + offset))})
+        // Starting the timer redefines the variable timerStart to give the
+        // correct total displayed time, taking into account pauses, even
+        // if that isn't the actual time of initially starting the timer.
+        timerStart = Date() - offset
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {timer in self.elapsedTimeLabel.setAttributedText(self.timeString(time: abs(self.timerStart.timeIntervalSinceNow)))})
     }
     
-    func resetTimer() {
+    @IBAction func resetTimer() {
         timer.invalidate()
         timerIsRunning = false
         savedTime = 0
         startButton.setTitle("Start")
         startButton.setBackgroundColor(startColor)
-        elapsedTimeLabel.setText(timeString(time: 0))
+        elapsedTimeLabel.setAttributedText(timeString(time: 0))
     }
 
     func pauseResumeTimer() {
         if timerIsRunning {
             timer.invalidate()
             // Reset the current saved time
-            savedTime = abs(timerStart.timeIntervalSinceNow) + savedTime
+            savedTime = abs(timerStart.timeIntervalSinceNow)
             startButton.setTitle("Resume")
             startButton.setBackgroundColor(startColor)
         } else {
@@ -57,26 +70,45 @@ final class WorkoutHUDController: HUDInterfaceController {
     }
 
 
-    //let timeInterval:TimeInterval = 0.05
     var timerIsRunning = false
+    var shouldResumeTimer = false
     var elapsedTime:TimeInterval = 0.0
+    
 
     
     override func willActivate() {
         super.willActivate()
-        resetTimer()
-//        update()
+        if shouldResumeTimer {
+            //  Time we are tracking has been continuing to change while
+            // controller out of view, update it:
+            elapsedTime = abs(timerStart.timeIntervalSinceNow)
+            elapsedTimeLabel.setAttributedText(timeString(time: elapsedTime))
+            startTimer(offset: elapsedTime)
+            shouldResumeTimer = false
+        }
+    }
+
+    override func willDisappear() {
+        if timerIsRunning {
+            shouldResumeTimer = true
+            timer.invalidate()
+        } else {
+            shouldResumeTimer = false
+        }
     }
     
-//    override func update() {
-//        super.update()
-//    }
-    
-    private func timeString(time:TimeInterval) -> String {
+    // Activation on first run:
+    override init() {
+        super.init()
+        resetTimer()
+    }
+
+    private func timeString(time:TimeInterval) -> NSAttributedString {
+        let timeFont = UIFont.monospacedDigitSystemFont(ofSize:  UIFont.preferredFont(forTextStyle: .largeTitle).pointSize, weight: UIFont.Weight.regular)
         let hours = Int(time) / 3600
         let minutes = Int(time - Double(hours) * 3600) / 60
         let seconds = time - Double(minutes) * 60 - Double(hours) * 3600
-        return String(format:"%02i:%02i:%04.1f",hours, minutes,seconds)
+        return NSAttributedString(string: String(format:"%02i:%02i:%02.0f",hours,minutes,seconds), attributes: [NSAttributedString.Key.font: timeFont])
     }
     
 
