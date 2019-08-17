cda4ea5245f720ed73cdbb837bea1017e96c98ab
elnjensen <ejensen1@swarthmore.edu>
2019-08-17 00:20:36 -0400

    Functional stopwatch

--- WatchApp Extension/Controllers/WorkoutHUDController.swift
+++ WatchApp Extension/Controllers/WorkoutHUDController.swift
@@ -18,29 +18,66 @@ final class WorkoutHUDController: HUDInterfaceController {
     @IBOutlet var startButton: WKInterfaceButton!
     
     @IBAction func toggleTimer() {
-        if timerIsRunning {
-            startButton.setTitle("Start")
-            elapsedTimeLabel.setText("00:00:00")
-        } else {
-            startButton.setTitle("Stop")
-            elapsedTimeLabel.setText("running...")
-        }
-         timerIsRunning = !timerIsRunning
+        pauseResumeTimer()
     }
-    //  var timer: Timer
     
+    var timerStart = NSDate()
+    var savedTime: TimeInterval = 0.0
+    var timer = Timer()
+    let startColor = UIColor(red: 35/255, green: 124/255, blue: 0/255, alpha: 1.0)
+    let stopColor = UIColor(red: 160/255, green: 40/255, blue: 0/255, alpha: 1.0)
+    
+    func startTimer(offset: TimeInterval) {
+        timerStart = NSDate() // initialize to now
+        timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: {timer in self.elapsedTimeLabel.setText(self.timeString(time: abs(self.timerStart.timeIntervalSinceNow) + offset))})
+    }
+    
+    func resetTimer() {
+        timer.invalidate()
+        timerIsRunning = false
+        savedTime = 0
+        startButton.setTitle("Start")
+        startButton.setBackgroundColor(startColor)
+        elapsedTimeLabel.setText(timeString(time: 0))
+    }
+
+    func pauseResumeTimer() {
+        if timerIsRunning {
+            timer.invalidate()
+            // Reset the current saved time
+            savedTime = abs(timerStart.timeIntervalSinceNow) + savedTime
+            startButton.setTitle("Resume")
+            startButton.setBackgroundColor(startColor)
+        } else {
+            startTimer(offset: savedTime)
+            startButton.setTitle("Pause")
+            startButton.setBackgroundColor(stopColor)
+        }
+        timerIsRunning = !timerIsRunning
+    }
+
+
+    //let timeInterval:TimeInterval = 0.05
     var timerIsRunning = false
-    
-    
+    var elapsedTime:TimeInterval = 0.0
+
     
     override func willActivate() {
         super.willActivate()
-        update()
+        resetTimer()
+//        update()
     }
     
-    override func update() {
-        super.update()
-      //  elapsedTimeLabel.setText("1")
+//    override func update() {
+//        super.update()
+//    }
+    
+    private func timeString(time:TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = Int(time - Double(hours) * 3600) / 60
+        let seconds = time - Double(minutes) * 60 - Double(hours) * 3600
+        return String(format:"%02i:%02i:%04.1f",hours, minutes,seconds)
     }
     
+
 }
