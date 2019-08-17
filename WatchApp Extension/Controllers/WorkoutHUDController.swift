3ac6b153b0a3cc463081a50b9ac8a93157688bde
elnjensen <ejensen1@swarthmore.edu>
2019-08-16 20:59:10 -0400

    Further layout of new page

--- WatchApp Extension/Controllers/WorkoutHUDController.swift
+++ WatchApp Extension/Controllers/WorkoutHUDController.swift
@@ -15,14 +15,32 @@ final class WorkoutHUDController: HUDInterfaceController {
     
     @IBOutlet var elapsedTimeLabel: WKInterfaceLabel!
     
+    @IBOutlet var startButton: WKInterfaceButton!
+    
+    @IBAction func toggleTimer() {
+        if timerIsRunning {
+            startButton.setTitle("Start")
+            elapsedTimeLabel.setText("00:00:00")
+        } else {
+            startButton.setTitle("Stop")
+            elapsedTimeLabel.setText("running...")
+        }
+         timerIsRunning = !timerIsRunning
+    }
+    //  var timer: Timer
+    
+    var timerIsRunning = false
+    
+    
+    
     override func willActivate() {
         super.willActivate()
         update()
     }
     
-    
     override func update() {
         super.update()
+      //  elapsedTimeLabel.setText("1")
     }
     
 }
