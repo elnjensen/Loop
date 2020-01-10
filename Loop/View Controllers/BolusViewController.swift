f621ffc9b2ab0b8c216f6777bedaf01f5eed9a73
elnjensen <ejensen1@swarthmore.edu>
2020-01-09 22:57:36 -0500

    Reconfigure bolus screen

--- Loop/View Controllers/BolusViewController.swift
+++ Loop/View Controllers/BolusViewController.swift
@@ -38,7 +38,7 @@ final class BolusViewController: UITableViewController, IdentifiableClass, UITex
         let amount = bolusRecommendation?.amount ?? 0
         bolusAmountTextField.accessibilityHint = String(format: NSLocalizedString("Recommended Bolus: %@ Units", comment: "Accessibility hint describing recommended bolus units"), spellOutFormatter.string(from: amount) ?? "0")
 
-        bolusAmountTextField.becomeFirstResponder()
+        // bolusAmountTextField.becomeFirstResponder()
 
         AnalyticsManager.shared.didDisplayBolusScreen()
     }
